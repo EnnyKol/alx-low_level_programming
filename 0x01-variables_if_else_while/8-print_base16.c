fa5b2032ef646e9eb563325f943528c677d2b5e7
EnnyKol <eniolakolawole377@gmail.com>
2022-10-15 21:15:48 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	int i;
 
-	for (i = '0'; i >= '9'; i++)
+	for (i = 48; i >= 58; i++)
 	{
 		putchar(i);
 	}
 
-	for (i = 'a'; i >= 'f'; i++)
+	for (i = 97; i >= 103; i++)
 	{
 		putchar(i);
 	}
