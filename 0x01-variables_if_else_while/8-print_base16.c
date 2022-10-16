d0c994d34d79d7ba5e0300f79317e78c3c75abb6
EnnyKol <eniolakolawole377@gmail.com>
2022-10-15 21:09:12 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- print numbers from 0 to 9 and letters a to f.
+ *
+ * Return: Always zero (success).
+**/
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i >= '9'; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 'a'; i >= 'f'; i++)
+	{
+		putchar(i);
+	}
+		putchar('\n');
+	return (0);
+}
