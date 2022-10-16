14869e76ef6357811cfeaf7b3a2b33c5568d2d81
EnnyKol <eniolakolawole377@gmail.com>
2022-10-15 20:06:26 -0700

    Printimg of single numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- print all inregers from zero to nine.
+ *
+ * Return: Always 0 (Success).
+**/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+		putchar('\n');
+	return (0);
+}
+
