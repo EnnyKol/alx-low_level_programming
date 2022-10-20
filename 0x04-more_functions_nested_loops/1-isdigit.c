401499f5ea464d8713c7e43044a431b76c4f2a62
EnnyKol <eniolakolawole377@gmail.com>
2022-10-19 23:52:11 -0700

    1-isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - chwcks for digit betwewn 0 to 9.
+ * @c: number to be checked.
+ * Return: 1 if the no is a digit, otherwise 0.
+**/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
