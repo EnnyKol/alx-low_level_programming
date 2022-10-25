f6178d86f50b041ce6180d5abeaa81239cfac26e
EnnyKol <eniolakolawole377@gmail.com>
2022-10-25 05:00:11 -0700

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - print a string.
+ * @str: string to be printed.
+**/
+
+void _puts(char *str)
+{
+	while (*str)
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+		_putchar(*str);
+	_putchar('\n');
+}
