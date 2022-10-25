7712be5e6488272384f17609759f3dc8f814828e
EnnyKol <eniolakolawole377@gmail.com>
2022-10-25 12:11:19 -0700

    puts2

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef
+#define
 void reset_to_98(int *n);
 int _putchar(char);
 void swap_int(int *a, int *b);
@@ -7,3 +9,5 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+
+#endif
