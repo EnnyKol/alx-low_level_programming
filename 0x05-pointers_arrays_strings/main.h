3a58111fe35be0400a7c92099321f2a3b7170fa4
EnnyKol <eniolakolawole377@gmail.com>
2022-10-25 14:21:15 -0700

    7-puts_half

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 void reset_to_98(int *n);
 int _putchar(char c);
 void swap_int(int *a, int *b);
@@ -9,5 +10,6 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
 
 #endif MAIN_H
